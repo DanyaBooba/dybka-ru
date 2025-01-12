b6165dbfd5cf72a5f84ebd8a79e0be4a261d64a0
Daniil Dybka <danil.dybko@gmail.com>
2025-01-12 20:54:30 +0300

    Работа над проектом

--- app/src/components/theme/theme.jsx
+++ app/src/components/theme/theme.jsx
@@ -10,12 +10,18 @@ export const theme = extendTheme({
                     themeColor: '#ffffff'
                 },
                 background: {
-                    body: '#FFFFFF',
+                    body: '#f4f6f7',
+                    surface: '#ffffff'
                 },
                 header: {
                     borderColor: 'hsla(220, 20%, 80%, 0.4)',
                     backgroundColor: 'rgba(252, 252, 252, 0.8)',
                     boxShadow: 'rgba(9, 11, 17, 0.03) 0px 4px 8px 0px, rgba(19, 23, 32, 0.03) 0px 7px 4px -5px',
+                },
+                showBlock: {
+                    borderColor: 'rgba(210, 219, 227, .5)',
+                    backgroundColor: '#ffffff',
+                    boxShadow: '0px 2px 16px 0px #e5ecef'
                 },
             },
         },
@@ -33,6 +39,11 @@ export const theme = extendTheme({
                     borderColor: 'rgba(92, 114, 133, 0.4)',
                     backgroundColor: 'rgba(14, 26, 36, 0.8)',
                     boxShadow: 'hsla(220, 60%, 70%, 0.03) 0px 4px 8px 0px, hsla(220, 55%, 75%, 0.03) 0px 7px 4px -5px',
+                },
+                showBlock: {
+                    borderColor: 'rgba(44, 81, 86, .5)',
+                    backgroundColor: '#244448',
+                    boxShadow: '0px 2px 16px 0px #183235',
                 },
             },
         },
--- app/src/pages/Main/PageIndex.tsx
+++ app/src/pages/Main/PageIndex.tsx
@@ -1,8 +1,54 @@
+import Sheet from '@mui/joy/Sheet';
+import Typography from "@mui/joy/Typography";
+import AsideHeader from '../../components/blocks/AsideHeader/AsideHeader';
+import {currentTheme} from '../../components/theme/theme';
+import Link from "@mui/joy/Link";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+function ShowBlock({title, showMore="", children}) {
+    return (
+        <Sheet sx={{
+                   borderColor: currentTheme().showBlock.borderColor,
+                   backgroundColor: currentTheme().showBlock.backgroundColor,
+                   boxShadow: currentTheme().showBlock.boxShadow,
+               }} className="sheetBlock">
+            <Typography level="h2">
+                {title}
+            </Typography>
+            {children}
+            {!!showMore && (
+                <div style={{ marginTop: '1rem' }}>
+                    <Link href={showMore} variant="soft" endDecorator={<ChevronRightIcon />}>
+                        Смотреть ещё
+                    </Link>
+                </div>
+            )}
+        </Sheet>
+    )
+}
+
 function PageIndex() {
     return (
-        <>
-            page index
-        </>
+        <AsideHeader>
+            <ShowBlock title="Мои проекты" showMore="/123">
+                123
+            </ShowBlock>
+            <ShowBlock title="Навыки в программировании" showMore="/123">
+                123
+            </ShowBlock>
+            <ShowBlock title="Достижения" showMore="/123">
+                123
+            </ShowBlock>
+            <ShowBlock title="Учеба" showMore="/123">
+                123
+            </ShowBlock>
+            <ShowBlock title="Фотографии с мероприятий" showMore="/123">
+                123
+            </ShowBlock>
+            <ShowBlock title="Контакты" showMore="/123">
+                123
+            </ShowBlock>
+        </AsideHeader>
     )
 }
 
