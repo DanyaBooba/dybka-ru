bf5778f61f64d694a2932d10d86281d15415d03e
Daniil Dybka <danil.dybko@gmail.com>
2025-02-02 17:00:13 +0300

    Работает над иконками

--- app/src/pages/Main/PageIndex/InnerBlockLanguages.tsx
+++ app/src/pages/Main/PageIndex/InnerBlockLanguages.tsx
@@ -1,5 +1,57 @@
 import Grid from '@mui/material/Grid2';
 import Card from '@mui/joy/Card';
+import Typography from '@mui/joy/Typography';
+
+function CPPIcon() {
+    return (
+        <svg fill="#00599c" role="img" width="32px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <title>C++</title>
+            <path
+                d="M22.394 6c-.167-.29-.398-.543-.652-.69L12.926.22c-.509-.294-1.34-.294-1.848 0L2.26 5.31c-.508.293-.923 1.013-.923 1.6v10.18c0 .294.104.62.271.91.167.29.398.543.652.69l8.816 5.09c.508.293 1.34.293 1.848 0l8.816-5.09c.254-.147.485-.4.652-.69.167-.29.27-.616.27-.91V6.91c.003-.294-.1-.62-.268-.91zM12 19.11c-3.92 0-7.109-3.19-7.109-7.11 0-3.92 3.19-7.11 7.11-7.11a7.133 7.133 0 016.156 3.553l-3.076 1.78a3.567 3.567 0 00-3.08-1.78A3.56 3.56 0 008.444 12 3.56 3.56 0 0012 15.555a3.57 3.57 0 003.08-1.778l3.078 1.78A7.135 7.135 0 0112 19.11zm7.11-6.715h-.79v.79h-.79v-.79h-.79v-.79h.79v-.79h.79v.79h.79zm2.962 0h-.79v.79h-.79v-.79h-.79v-.79h.79v-.79h.79v.79h.79z"/>
+        </svg>
+    )
+}
+
+function CssIcon() {
+    return (
+        <svg fill="#1572b6" width="32px" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>CSS3</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z"/></svg>
+    )
+}
+
+function HTMLIcon() {
+    return (
+        <svg fill="#e34f26" width="32px" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>HTML5</title><path d="M1.5 0h21l-1.91 21.563L11.977 24l-8.564-2.438L1.5 0zm7.031 9.75l-.232-2.718 10.059.003.23-2.622L5.412 4.41l.698 8.01h9.126l-.326 3.426-2.91.804-2.955-.81-.188-2.11H6.248l.33 4.171L12 19.351l5.379-1.443.744-8.157H8.531z"/></svg>
+    )
+}
+
+function JavaIcon() {
+    return (
+        <svg width="32px" height="32px" viewBox="0 0 271 367" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M87.4296 283.241C87.4296 283.241 73.4501 291.371 97.3784 294.122C126.367 297.429 141.182 296.955 173.128 290.909C173.128 290.909 181.527 296.175 193.256 300.736C121.643 331.428 31.1813 298.958 87.4296 283.241ZM78.6789 243.19C78.6789 243.19 62.9995 254.796 86.9455 257.273C117.912 260.468 142.367 260.729 184.683 252.581C184.683 252.581 190.536 258.514 199.739 261.759C113.154 287.078 16.7147 263.756 78.6789 243.19Z" fill="#5382A1"/><path d="M152.45 175.25C170.096 195.565 147.814 213.847 147.814 213.847C147.814 213.847 192.619 190.717 172.042 161.753C152.824 134.743 138.086 121.322 217.871 75.0504C217.871 75.0504 92.6357 106.328 152.45 175.25Z" fill="#F8981D"/><path d="M247.165 312.866C247.165 312.866 257.51 321.39 235.771 327.984C194.435 340.506 63.7262 344.288 27.4158 328.483C14.3631 322.805 38.8405 314.925 46.5402 313.271C54.5702 311.53 59.159 311.854 59.159 311.854C44.6431 301.628 -34.6658 331.933 18.874 340.612C164.885 364.291 285.038 329.949 247.165 312.866ZM94.1522 201.692C94.1522 201.692 27.6652 217.484 70.6075 223.219C88.739 225.646 124.884 225.097 158.552 222.276C186.067 219.955 213.696 215.02 213.696 215.02C213.696 215.02 203.994 219.175 196.974 223.968C129.458 241.725 -0.971188 233.464 36.5777 215.301C68.3328 199.951 94.1522 201.692 94.1522 201.692ZM213.422 268.36C282.055 232.695 250.322 198.422 228.172 203.039C222.743 204.169 220.323 205.148 220.323 205.148C220.323 205.148 222.338 201.991 226.187 200.624C270.006 185.219 303.706 246.06 212.042 270.157C212.042 270.158 213.104 269.209 213.422 268.36Z" fill="#5382A1"/><path d="M172.042 0.333313C172.042 0.333313 210.053 38.3564 135.991 96.8253C76.6014 143.727 122.448 170.47 135.966 201.024C101.3 169.746 75.8589 142.212 92.9266 116.586C117.978 78.9688 187.378 60.7307 172.042 0.333313Z" fill="#F8981D"/><path d="M100.896 365.002C166.776 369.22 267.942 362.663 270.338 331.49C270.338 331.49 265.732 343.308 215.892 352.692C159.662 363.274 90.3108 362.039 49.1778 355.257C49.1778 355.257 57.5982 362.226 100.896 365.002Z" fill="#5382A1"/></svg>
+    )
+}
+
+function JSIcon() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_1_10)">
+            <rect width="24" height="24" fill="white"/>
+            <path d="M0 0H24V24H0V0ZM22.034 18.276C21.859 17.181 21.146 16.261 19.031 15.403C18.295 15.058 17.477 14.818 17.234 14.263C17.143 13.933 17.129 13.753 17.188 13.558C17.338 12.912 18.103 12.718 18.703 12.898C19.093 13.018 19.453 13.318 19.679 13.798C20.713 13.122 20.713 13.122 21.434 12.673C21.164 12.253 21.03 12.072 20.848 11.893C20.218 11.188 19.379 10.828 18.014 10.859L17.309 10.948C16.633 11.113 15.989 11.473 15.599 11.953C14.459 13.244 14.788 15.494 16.168 16.424C17.533 17.444 19.529 17.668 19.784 18.629C20.024 19.799 18.914 20.174 17.818 20.039C17.007 19.859 16.558 19.453 16.063 18.703L14.233 19.754C14.443 20.234 14.683 20.443 15.043 20.863C16.783 22.619 21.133 22.529 21.914 19.859C21.943 19.769 22.154 19.154 21.988 18.209L22.034 18.276ZM13.051 11.031H10.803C10.803 12.969 10.794 14.895 10.794 16.836C10.794 18.068 10.857 19.199 10.656 19.547C10.326 20.236 9.476 20.148 9.09 20.027C8.694 19.831 8.493 19.561 8.26 19.172C8.197 19.067 8.15 18.976 8.133 18.976L6.308 20.101C6.613 20.731 7.058 21.273 7.632 21.618C8.487 22.128 9.636 22.293 10.839 22.023C11.622 21.797 12.297 21.332 12.65 20.612C13.16 19.682 13.052 18.542 13.047 17.266C13.059 15.212 13.047 13.157 13.047 11.087L13.051 11.031Z" fill="#F7DF1E"/>
+            </g>
+            <defs>
+            <clipPath id="clip0_1_10">
+            <rect width="24" height="24" fill="white"/>
+            </clipPath>
+            </defs>
+            </svg>
+
+    )
+}
+
+function MDIcon() {
+    return (
+        <svg fill="#000000" width="32px" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Markdown</title><path d="M22.27 19.385H1.73A1.73 1.73 0 010 17.655V6.345a1.73 1.73 0 011.73-1.73h20.54A1.73 1.73 0 0124 6.345v11.308a1.73 1.73 0 01-1.73 1.731zM5.769 15.923v-4.5l2.308 2.885 2.307-2.885v4.5h2.308V8.078h-2.308l-2.307 2.885-2.308-2.885H3.46v7.847zM21.232 12h-2.309V8.077h-2.307V12h-2.308l3.461 4.039z"/></svg>
+    )
+}
 
 function InnerBlockLanguages() {
     const gridSize = {
@@ -12,48 +64,66 @@ function InnerBlockLanguages() {
     return (
         <Grid container spacing={2}>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    C++
+                <Card variant="soft">
+                    <Typography startDecorator={<CPPIcon/>} sx={{justifyContent: 'center'}}>
+                        C++
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    JavaScript
+                <Card variant="soft">
+                    <Typography startDecorator={<JSIcon/>} sx={{justifyContent: 'center'}}>
+                        JavaScript
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    TypeScript
+                <Card variant="soft">
+                    <Typography startDecorator={<CPPIcon/>} sx={{justifyContent: 'center'}}>
+                        TypeScript
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    HTML5
+                <Card variant="soft">
+                    <Typography startDecorator={<HTMLIcon/>} sx={{justifyContent: 'center'}}>
+                        HTML5
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    CSS3
+                <Card variant="soft">
+                    <Typography startDecorator={<CssIcon/>} sx={{justifyContent: 'center'}}>
+                        CSS3
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    PHP
+                <Card variant="soft">
+                    <Typography startDecorator={<CPPIcon/>} sx={{justifyContent: 'center'}}>
+                        PHP
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    Markdown
+                <Card variant="soft">
+                    <Typography startDecorator={<MDIcon/>} sx={{justifyContent: 'center'}}>
+                        Markdown
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    Python
+                <Card variant="soft">
+                    <Typography startDecorator={<CPPIcon/>} sx={{justifyContent: 'center'}}>
+                        Python
+                    </Typography>
                 </Card>
             </Grid>
             <Grid size={gridSize}>
-                <Card variant="soft" sx={{ textAlign: 'center' }}>
-                    Java
+                <Card variant="soft">
+                    <Typography startDecorator={<JavaIcon/>} sx={{justifyContent: 'center'}}>
+                        Java
+                    </Typography>
                 </Card>
             </Grid>
         </Grid>
